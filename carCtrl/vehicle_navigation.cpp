e17596608f075a357442af4248a7187dd49b9249
Alexander Jackson <jacksa2@mcmaster.ca>
2017-02-01 14:31:39 -0500

    Created vehicle navigation c++ files

---
+++ carCtrl/vehicle_navigation.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+
+#include "vehicle_navigation.h"
+#include "vic_types.h"
+
+// Constants
+double MAX_SPEED = 50
+double MAX_ANGLE = 50
+
