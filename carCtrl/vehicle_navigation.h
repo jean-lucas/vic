99e3458f0165687c1224a9500ff2a806f338ed12
Alexander Jackson <jacksa2@mcmaster.ca>
2017-02-04 13:59:34 -0500

    Vehicle Navigation

--- carCtrl/vehicle_navigation.h
+++ carCtrl/vehicle_navigation.h
@@ -1,10 +1,10 @@
-#ifndef VEHICLE_NAVIGATOIN__H
-#define VEHICLE_NAVIGATION__H
+#ifndef VEHICLE_NAVIGATION_H
+#define VEHICLE_NAVIGATION_H
 
 //Constants
-double MAX_SPEED = 1.4; //metres per second
-double MAX_ANGLE = 45; //degrees
-double ANGLE_THRESHOLD =  15; //degrees
+const double MAX_SPEED = 1.4; //metres per second
+const double MAX_ANGLE = 45; //degrees
+const double ANGLE_THRESHOLD =  15; //degrees
 
 int update_navigation(struct *ImageData, struct *CarStatus);
 
