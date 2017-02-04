f4990b8c73d61b80ba0fbc9bbcb34c55bb2e88a8
Alexander Jackson <jacksa2@mcmaster.ca>
2017-02-04 13:39:22 -0500

    Vehicle Navigation

--- carCtrl/vehicle_navigation.cpp
+++ carCtrl/vehicle_navigation.cpp
@@ -4,8 +4,35 @@
 
 #include "vehicle_navigation.h"
 #include "vic_types.h"
+#include "servo_controller.h"
 
-// Constants
-double MAX_SPEED = 50
-double MAX_ANGLE = 50
-
+int update_navigation(ImageData *img, CarStatus *car){
+	
+	double speed_ok = 0;
+	double angle_ok = 0;
+	
+	//TODO: Update vehicle speed
+	speed_ok = 1;
+	
+	//Update steering angle
+	double angle_diff_abs = abs(img->avg_left_angle - img->avg_right_angle);
+	
+	if(angle_diff_abs >= ANGLE_THRESHOLD){
+		
+		double angle_diff =  img->avg_left_angle - img->avg_right_angle;
+		double new_angle = angle_diff/2;
+		
+		if(angle_diff > MAX_ANGLE){
+			new_angle = MAX_ANGLE;
+		}else if(angle_diff < -1*MAX_ANGLE){
+			new_angle = -1*MAX_ANGLE;
+		}
+		
+		car->current_wheel_angle = new_angle;
+		set_angle(new_angle);
+		angle_ok = 1;
+		
+		return speed_ok*angle_ok;
+	}
+	
+}
--- carCtrl/vehicle_navigation.h
+++ carCtrl/vehicle_navigation.h
@@ -1,3 +1,11 @@
 #ifndef VEHICLE_NAVIGATOIN_H
 #define VEHICLE_NAVIGATION_H
 
+//Constants
+double MAX_SPEED = 1.4; //metres per second
+double MAX_ANGLE = 45; //degrees
+double ANGLE_THRESHOLD =  15; //degrees
+
+int update_navigation(struct *ImageData, struct *CarStatus);
+
+#endif
