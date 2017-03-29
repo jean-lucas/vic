0628049cedeb0e3cc32ca770e8c342b43706901b
Radhika21 <radhika.r.sharma@hotmail.com>
2017-03-29 13:53:19 -0400

    edits

--- icCtrl/IC_Main.py
+++ icCtrl/IC_Main.py
@@ -1,6 +1,6 @@
 # external class dependencies
-from TrafficController import TrafficController
-from VehicleDetection import VehicleDetection
+#from TrafficController import TrafficController
+#from VehicleDetection import VehicleDetection
 from Car import Car
 from Communication import Communication 
 
@@ -13,8 +13,8 @@ import random
 # Main class
 class IC_Main(object):
 
-    traffic = TrafficController()
-    detect = VehicleDetection()
+    #traffic = TrafficController()
+    #detect = VehicleDetection()
     communication = Communication()
 
     intersection_cars = [0 for i in range(4)]
@@ -48,33 +48,33 @@ class IC_Main(object):
                 #print "Message count: %d. " % test
                 self.test+=1
                 #setattr(car, 'message_to_car',"GO %d" % test)
-                time.sleep(1)
-                start = 0
-                current_car_index = -1
+                time.sleep(0.3)
+                #start = 0
+                #current_car_index = -1
 
-                for i in range(len(self.intersection_cars)):
-                    if(self.intersection_cars[i]==0):
-                        self.intersection_cars[i] = self.car
-                        current_car_index = i
-                        break
-
-                while (getattr(self.intersection_cars[current_car_index], 'proceed_now') == False):
-                    self.check_intersection_state()
-                    print self.intersection_clear
-
-                    if(self.intersection_clear):
-                        setattr(self.intersection_cars[current_car_index],'proceed_now',True)
-                    else:
-                        for i in range(len(self.intersection_cars)):
-                            if(self.intersection_cars[i]!=0 and i!=current_car_index):
-                                if(getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_from') or getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_to')):
-                                    setattr(self.intersection_cars[current_car_index],'proceed_now',True)
-                                    break   #this break only works if we have 2 cars on the track (more efficient)
-                                #else:
-                                #    setattr(self.intersection_cars[current_car_index],'proceed_now',False)
-
-
-                #current car's proceed_now is true, then add car to communication's proceed queue
+##                for i in range(len(self.intersection_cars)):
+##                    if(self.intersection_cars[i]==0):
+##                        self.intersection_cars[i] = self.car
+##                        current_car_index = i
+##                        break
+##
+##                while (getattr(self.intersection_cars[current_car_index], 'proceed_now') == False):
+##                    self.check_intersection_state()
+##                    print self.intersection_clear
+##
+##                    if(self.intersection_clear):
+##                        setattr(self.intersection_cars[current_car_index],'proceed_now',True)
+##                    else:
+##                        for i in range(len(self.intersection_cars)):
+##                            if(self.intersection_cars[i]!=0 and i!=current_car_index):
+##                                if(getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_from') or getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_to')):
+##                                    setattr(self.intersection_cars[current_car_index],'proceed_now',True)
+##                                    break   #this break only works if we have 2 cars on the track (more efficient)
+##                                #else:
+##                                #    setattr(self.intersection_cars[current_car_index],'proceed_now',False)
+##
+##
+##                #current car's proceed_now is true, then add car to communication's proceed queue
                 self.communication.proceed_enqueue(self.car)
                 
         
