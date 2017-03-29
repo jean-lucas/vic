815be301fccca45b0ffa020d655e6b2715126dff
Mathew Hobers <hobersme@mcmaster.ca>
2017-03-29 15:11:20 -0400

    idk

--- icCtrl/IC_Main.py
+++ icCtrl/IC_Main.py
@@ -1,8 +1,7 @@
 # external class dependencies
-from TrafficController import TrafficController
 from VehicleDetection import VehicleDetection
 from Car import Car
-from Communication import Communication 
+from Communication import Communication
 
 
 
@@ -13,7 +12,6 @@ import random
 # Main class
 class IC_Main(object):
 
-    traffic = TrafficController()
     detect = VehicleDetection()
     communication = Communication()
 
@@ -33,7 +31,7 @@ class IC_Main(object):
 
         while True:
             
-            #self.check_intersection_state()
+            #self.update_intersection_state()
             #self.traffic.test()
             
             
@@ -43,38 +41,38 @@ class IC_Main(object):
                 
                 self.car = self.communication.arrival_deque()
 
-                print "Car direction_from: %s" % (getattr(self.car,'direction_from'))
+                #print "Car direction_from: %s" % (getattr(self.car,'direction_from'))
                
                 #print "Message count: %d. " % test
                 self.test+=1
                 #setattr(car, 'message_to_car',"GO %d" % test)
                 time.sleep(1)
-                start = 0
-                current_car_index = -1
+                # start = 0
+                # current_car_index = -1
 
-                for i in range(len(self.intersection_cars)):
-                    if(self.intersection_cars[i]==0):
-                        self.intersection_cars[i] = self.car
-                        current_car_index = i
-                        break
+                # for i in range(len(self.intersection_cars)):
+                #     if(self.intersection_cars[i]==0):
+                #         self.intersection_cars[i] = self.car
+                #         current_car_index = i
+                #         break
 
-                while (getattr(self.intersection_cars[current_car_index], 'proceed_now') == False):
-                    self.check_intersection_state()
-                    print self.intersection_clear
+                # while (getattr(self.intersection_cars[current_car_index], 'proceed_now') == False):
+                #     self.update_intersection_state(current_car_index)
 
-                    if(self.intersection_clear):
-                        setattr(self.intersection_cars[current_car_index],'proceed_now',True)
-                    else:
-                        for i in range(len(self.intersection_cars)):
-                            if(self.intersection_cars[i]!=0 and i!=current_car_index):
-                                if(getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_from') or getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_to')):
-                                    setattr(self.intersection_cars[current_car_index],'proceed_now',True)
-                                    break   #this break only works if we have 2 cars on the track (more efficient)
-                                #else:
-                                #    setattr(self.intersection_cars[current_car_index],'proceed_now',False)
+                #     if(self.intersection_clear):
+                #         setattr(self.intersection_cars[current_car_index],'proceed_now',True)
+                #     else:
+                #         for i in range(len(self.intersection_cars)):
+                #             if(self.intersection_cars[i]!=0 and i!=current_car_index):
+                #                 if(getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_from') or getattr(self.intersection_cars[current_car_index], 'direction_from')==getattr(self.intersection_cars[i], 'direction_to')):
+                #                     setattr(self.intersection_cars[current_car_index],'proceed_now',True)
+                #                     #break   #this break only works if we have 2 cars on the track (more efficient)
+                #                 else:
+                #                     setattr(self.intersection_cars[current_car_index],'proceed_now',False)
+                #                     break
 
 
-                #current car's proceed_now is true, then add car to communication's proceed queue
+                # #current car's proceed_now is true, then add car to communication's proceed queue
                 self.communication.proceed_enqueue(self.car)
                 
         
@@ -84,8 +82,16 @@ class IC_Main(object):
         #self.arrivals.appendleft(car)
 
 
-    def check_intersection_state(self):
-        intersection_clear = True
+    def update_intersection_state(self,current_car_index):
+        #update intersection_cars array from camera info for cars leaving
+
+        #check if intersection is empty
+        for i in range(len(self.intersection_cars)):
+            if(i!=current_car_index and self.intersection_cars[i]!=0):
+                self.intersection_clear = False
+                break
+            else:
+                self.intersection_clear = True
         #print self.detect.update_intersection()
         #this function will retrieve state of intersection and update intersection_cars array for any cars leaving intersection
         
