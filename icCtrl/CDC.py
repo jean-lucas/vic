d8ac95d144af2742c0b6863de6dee32dcf1fb643
Radhika21 <radhika.r.sharma@hotmail.com>
2017-04-12 21:28:57 -0400

    edits

--- icCtrl/CDC.py
+++ icCtrl/CDC.py
@@ -13,7 +13,6 @@ class CDC(object):
     intersection_state = [0,0,0,0,0,0,0,0]
 
     def __init__(self):
-        
         #recieving request 
         color_detect = threading.Thread(target = self.color_detection, args=())
         color_detect.start()
@@ -21,9 +20,11 @@ class CDC(object):
     def color_detection(self):
         
         cap = cv2.VideoCapture(0)
+        #time.sleep(5)
         firstFrame = None
+        counter = 0
         while (1) :
-            #time.sleep(1)
+            time.sleep(0.5)
             _, frame = cap.read()
 
             #cv2.imshow("frame", frame)
@@ -61,8 +62,8 @@ class CDC(object):
 
             #define range of yellow color in HSV
 
-            lowerYellow = np.array([20,100,100], dtype = np.uint8)
-            upperYellow = np.array([30,255,255], dtype = np.uint8)
+            lowerYellow = np.array([15,100,100], dtype = np.uint8)
+            upperYellow = np.array([35,255,255], dtype = np.uint8)
 
             #define range of blue color in HSV
 
@@ -81,7 +82,7 @@ class CDC(object):
             maskRED0 = cv2.inRange(hsv0, lowerRed, upperRed)
             resRED0 = cv2.bitwise_and(fr0,fr0, mask = maskRED0)
             NumRedPixHSV0 = cv2.countNonZero(maskRED0)
-            print 'NumRedPixHSV0 : %d ' %NumRedPixHSV0
+            #print 'NumRedPixHSV0 : %d ' %NumRedPixHSV0
             
             #cv2.imshow('maskRED0',maskRED0)
             #cv2.imshow('resRED0',resRED0)
@@ -91,7 +92,7 @@ class CDC(object):
             maskYELW0 = cv2.inRange(hsv0, lowerYellow, upperYellow)
             resYELW0 = cv2.bitwise_and(fr0, fr0, mask = maskYELW0)
             NumYelwPixHSV0 = cv2.countNonZero(maskYELW0)
-            print 'NumYelwPixHSV0 : %d' %NumYelwPixHSV0
+            #print 'NumYelwPixHSV0 : %d' %NumYelwPixHSV0
                     
             #cv2.imshow('maskYELW0',maskYELW0)
             #cv2.imshow('resYELW0',resYELW0)
@@ -101,7 +102,7 @@ class CDC(object):
             maskRED3 = cv2.inRange(hsv3, lowerRed, upperRed)
             resRED3 = cv2.bitwise_and(fr3,fr3, mask = maskRED3)
             NumRedPixHSV3 = cv2.countNonZero(maskRED3)
-            print 'NumRedPixHSV3 %d: ' %NumRedPixHSV3
+            #print 'NumRedPixHSV3 %d: ' %NumRedPixHSV3
             
             #cv2.imshow('maskRED3',maskRED3)
             #cv2.imshow('resRED3',resRED3)
@@ -111,7 +112,7 @@ class CDC(object):
             maskYELW3 = cv2.inRange(hsv3, lowerYellow, upperYellow)
             resYELW3 = cv2.bitwise_and(fr3, fr3, mask = maskYELW3)
             NumYelwPixHSV3 = cv2.countNonZero(maskYELW3)
-            print 'NumYelwPixHSV3 : %d ' %NumYelwPixHSV3
+            #print 'NumYelwPixHSV3 : %d ' %NumYelwPixHSV3
 
             #cv2.imshow('maskYELW3',maskYELW3)
             #cv2.imshow('resYELW3',resYELW3)  
@@ -123,7 +124,7 @@ class CDC(object):
             maskBLUE2 = cv2.inRange(hsv2, lowerBlue, upperBlue)
             resBLUE2 = cv2.bitwise_and(fr2,fr2, mask = maskBLUE2)
             NumBluePixHSV2 = cv2.countNonZero(maskBLUE2)
-            print 'NumBluePixHSV2 : %d' %NumBluePixHSV2
+            #print 'NumBluePixHSV2 : %d' %NumBluePixHSV2
             
             #cv2.imshow('maskBLUE2',maskBLUE2)
             #cv2.imshow('resBLUE2',resBLUE2)
@@ -133,7 +134,7 @@ class CDC(object):
             maskGREEN2 = cv2.inRange(hsv2, lowerGreen, upperGreen)
             resGREEN2 = cv2.bitwise_and(fr2, fr2, mask = maskGREEN2)
             NumGrnPixHSV2 = cv2.countNonZero(maskGREEN2)
-            print 'NumGrnPixHSV2 : %d'  %NumGrnPixHSV2
+            #print 'NumGrnPixHSV2 : %d'  %NumGrnPixHSV2
                     
             #cv2.imshow('maskGREEN2',maskGREEN2)
             #cv2.imshow('resGREEN2',resGREEN2)
@@ -145,7 +146,7 @@ class CDC(object):
             maskBLUE1 = cv2.inRange(hsv1, lowerBlue, upperBlue)
             resBLUE1 = cv2.bitwise_and(fr1,fr1, mask = maskBLUE1)
             NumBLUEPixHSV1 = cv2.countNonZero(maskBLUE1)
-            print 'NumBLUEPixHSV1 : %d' %NumBLUEPixHSV1
+            #print 'NumBLUEPixHSV1 : %d' %NumBLUEPixHSV1
             
             #cv2.imshow('maskBLUE1',maskBLUE1)
             #cv2.imshow('resBLUE1',resBLUE1)
@@ -155,7 +156,7 @@ class CDC(object):
             maskGRN1 = cv2.inRange(hsv1, lowerGreen, upperGreen)
             resGRN1 = cv2.bitwise_and(fr1, fr1, mask = maskGRN1)
             NumGRNPixHSV1 = cv2.countNonZero(maskGRN1)
-            print 'NumGRNPixHSV1 : %d ' %NumGRNPixHSV1  
+            #print 'NumGRNPixHSV1 : %d ' %NumGRNPixHSV1  
                     
             #cv2.imshow('maskGRN1',maskGRN1)
             #cv2.imshow('resGRN1',resGRN1)
@@ -180,7 +181,7 @@ class CDC(object):
         ##        NumYelwPixHSV3
 
             upper_bound = 3000
-            lower_bound = 2900
+            lower_bound = 2300
             
         #Incoming positions
             
@@ -188,7 +189,7 @@ class CDC(object):
             
             if (NumYelwPixHSV0 < lower_bound) & (last_stateHSV0C == 'Unoccupied'):
                 last_stateHSV0C = 'Occupied'   
-                self.intersection_state[0] = 1
+                self.intersection_state[4] = 1
             elif NumYelwPixHSV0 > upper_bound:
                 last_stateHSV0C = 'Unoccupied'
 
@@ -196,7 +197,7 @@ class CDC(object):
 
             if (NumBLUEPixHSV1 < lower_bound) & (last_stateHSV1C == 'Unoccupied'):
                 last_stateHSV1C = 'Occupied'   
-                self.intersection_state[1] = 1
+                self.intersection_state[5] = 1
             elif NumBLUEPixHSV1 > upper_bound:
                 last_stateHSV1C = 'Unoccupied'
 
@@ -204,7 +205,7 @@ class CDC(object):
 
             if (NumGrnPixHSV2 < lower_bound) & (last_stateHSV2C == 'Unoccupied'):
                 last_stateHSV2C = 'Occupied'   
-                self.intersection_state[2] = 1
+                self.intersection_state[6] = 1
             elif NumGrnPixHSV2 > upper_bound:
                 last_stateHSV2C = 'Unoccupied'
 
@@ -212,7 +213,7 @@ class CDC(object):
 
             if (NumRedPixHSV3 < lower_bound) & (last_stateHSV3C == 'Unoccupied'):
                 last_stateHSV3C = 'Occupied'   
-                self.intersection_state[3] = 1
+                self.intersection_state[7] = 1
             elif NumRedPixHSV3 > upper_bound:
                 last_stateHSV3C = 'Unoccupied'
 
@@ -220,7 +221,7 @@ class CDC(object):
 
             if (NumRedPixHSV0 < lower_bound) & (last_stateHSV0G == 'Unoccupied'):
                 last_stateHSV0G = 'Occupied'   
-                self.intersection_state[4] = 1
+                self.intersection_state[0] = 1
             elif NumRedPixHSV0 > upper_bound:
                 last_stateHSV0G = 'Unoccupied'
 
@@ -228,7 +229,7 @@ class CDC(object):
 
             if (NumGRNPixHSV1 < lower_bound) & (last_stateHSV1G == 'Unoccupied'):
                 last_stateHSV1G = 'Occupied'   
-                self.intersection_state[5] = 1
+                self.intersection_state[1] = 1
             elif NumGRNPixHSV1 > upper_bound:
                 last_stateHSV1G = 'Unoccupied'
 
@@ -236,17 +237,23 @@ class CDC(object):
 
             if (NumBluePixHSV2 < lower_bound) & (last_stateHSV2G == 'Unoccupied'):
                 last_stateHSV2G = 'Occupied'   
-                self.intersection_state[6] = 1
-            elif NumBluePixHSV2 > some_threshold:
+                self.intersection_state[2] = 1
+            elif NumBluePixHSV2 > upper_bound:
                 last_stateHSV2G = 'Unoccupied'
 
             # quadrant 4
 
             if (NumYelwPixHSV3 < lower_bound) & (last_stateHSV3G == 'Unoccupied'):
                 last_stateHSV3G = 'Occupied'   
-                self.intersection_state[7] = 1
+                self.intersection_state[3] = 1
             elif NumYelwPixHSV3 > upper_bound:
                 last_stateHSV3G = 'Unoccupied'
+
+            
+
+            if counter < 10:
+                self.intersection_state = [0,0,0,0,0,0,0,0]
+            #print self.intersection_state
                             
 
             k = cv2.waitKey(5) & 0xFF
@@ -255,10 +262,13 @@ class CDC(object):
         ##
         ##    cap.release()
         ##    cv2.destroyAllWindows()
+            counter = counter + 1 
 
     def get_intersection_state(self):
-        new_intersection_state = x.intersection_state
-        x.intersection_state = [0,0,0,0,0,0,0,0]
+        print 'get_intersection_state called'
+        new_intersection_state = self.intersection_state
+        self.intersection_state = [0,0,0,0,0,0,0,0]
+        
         return new_intersection_state
         
 
